c5f8077e4910144552a6281a976447972d29c1e0
Aldinn <kferdoush617@gmail.com>
2022-06-26 02:53:23 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./*.{html,js}"
+
+
+],
+  theme: {
+    extend: {},
+    // fontFamily: {
+    //   'sans': ['ui-sans-serif', 'system-ui', ...],
+    //   'serif': ['ui-serif', 'Georgia', ...],
+    //   'mono': ['ui-monospace', 'SFMono-Regular', ...],
+    //   'display': ['Oswald'],
+    //   'h1': ['"Lato"', 'sans-serif',.]
+    // },
+    
+  },
+  plugins: [],
+}
